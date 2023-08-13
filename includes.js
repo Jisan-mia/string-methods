62de65c805558ee5e663f49ecff5ae826d3d0a49
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-08-13 19:49:20 +0600

    String.includes() method

---
+++ includes.js
@@ -0,0 +1,8 @@
+// String.includes(searchString) is used whenever we want to see or determine
+// if our string is found within another string, if found returns true else false
+
+// examples
+const string = 'The brown fox jumps over the lazy dog undefined'
+console.log(string.includes('fox')) // true
+console.log(string.includes('Fox')) // false
+console.log(string.includes()) // true
