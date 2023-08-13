67e683c5e2dab0a671e06622a837415b83f793ed
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-08-13 19:57:11 +0600

    String.startsWith() method

---
+++ startsWith.js
@@ -0,0 +1,7 @@
+// String.startsWith(searchString) is used whenever we ant to check if a string starts with another string or not
+
+
+// examples
+const string = 'Playing cricket is good for health'
+console.log(string.startsWith('Playing')) // true
+console.log(string.startsWith('cricket')) // false
