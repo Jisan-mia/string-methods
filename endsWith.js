fd69796c75201c7aaad20ec2253fd6deb1dd7ba1
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-08-14 20:02:10 +0600

    String.endsWith() method

---
+++ endsWith.js
@@ -0,0 +1,6 @@
+// String.endsWith(searchString) checks whether a string ends with another string or not
+
+// example
+const string = 'The cat is beautiful!'
+console.log(string.endsWith("beautiful!")); // true
+console.log(string.endsWith("beautiful")); // false
