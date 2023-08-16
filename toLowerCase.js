7fde21a8cf89d0532e1f5d45ba4771405695ce19
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-08-16 12:19:02 +0600

    toLowerCase, toLowerCase method

---
+++ toLowerCase.js
@@ -0,0 +1,6 @@
+// String.toLowerCase() is used whenever we want to all 
+// the alphabetic character to be lower case or small letter
+
+// examples
+const string = 'My name is Jisan'
+console.log(string.toLowerCase()) // my name is jisan
---
+++ toUpperCase.js
@@ -0,0 +1,6 @@
+// String.toUpperCase() is used whenever we want to all
+// the alphabetic character to be upper case or capital letter
+
+// examples
+const string = "my name is jisan";
+console.log(string.toUpperCase()); // MY NAME IS JISAN
