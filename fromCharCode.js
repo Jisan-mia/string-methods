a54d88642a8de58950d02d8b6e85a91317c9a5d7
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-08-16 12:14:35 +0600

    fromCharCode, fromCodePoint methods

---
+++ fromCharCode.js
@@ -0,0 +1,8 @@
+// String.fromCharCode(num) is used whenever we want to get 
+// actual value from a unicode
+
+
+// examples
+console.log(String.fromCharCode(79)) // 'O'
+console.log(String.fromCharCode(107)) // 'k'
+console.log(String.fromCharCode(55357)); // �
---
+++ fromCodePoint.js
@@ -0,0 +1,8 @@
+// String.fromCodePoint(num) is used whenever we want to get
+// actual value from a unicode
+
+// examples
+console.log(String.fromCodePoint(79)); // 'O'
+console.log(String.fromCodePoint(107)); // 'k'
+console.log(String.fromCodePoint(128077)); // 👍
+console.log(String.fromCodePoint(56397)); // �
